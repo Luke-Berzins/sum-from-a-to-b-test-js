8e55961746f227fee20f27955ffab3c2dc475df8
luke-berzins <lukeberzins16@gmail.com>
2020-11-28 01:52:58 +0000

    strech recursion a to b

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+  if (fromN > toN) {
+    return 0;
+  }
+  return fromN + sum(fromN + 1, toN);
+};
 
+
+console.log(sum(4, 8))
+
+
+//
 module.exports = sum;
